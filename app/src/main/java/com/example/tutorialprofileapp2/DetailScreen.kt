2fd4a82a2f184d154496ca400a76bb3551d599ff
Takumi Fukuzawa <forthune.0517@gmail.com>
2023-08-13 22:47:41 +0900

    不要なimport削除

--- app/src/main/java/com/example/tutorialprofileapp2/DetailScreen.kt
+++ app/src/main/java/com/example/tutorialprofileapp2/DetailScreen.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
